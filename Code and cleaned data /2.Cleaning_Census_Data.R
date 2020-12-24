ebf711af37bdd1efdc761626a9a3b6d6f1dd1fc3
hiraethwly <wlyleyi@gmail.com>
2020-12-24 07:31:35 -0500

    Upload Final Project

---
+++ Code and cleaned data /2.Cleaning_Census_Data.R
@@ -0,0 +1,41 @@
+#### Preamble ####
+# Purpose: Cleaning Census data
+# Author: Leyi Wang (1006318682)
+# Data: 16 December 2020
+# License: MIT
+# Pre-requisites: 
+# - Need to have downloaded the data and save it into the 
+#   same folder as this document
+
+
+#### Workspace setup ####
+setwd("~/Desktop/R studio/STA304/Final Project")
+gss <- read.csv("gss.csv")
+library(tidyverse)
+library(plyr)
+
+
+census_data <- select(gss, c(sex, province, education,income_respondent))
+head(census_data)
+census_data<-na.omit(census_data)
+census_data<-rename(census_data, c("sex"="gender","income_respondent"="income"))
+
+#income
+census_data$income<-revalue(census_data$income, c("Less than $25,000"="Low-income",
+                                           "$25,000 to $49,999"="Low-income",
+                                           "$50,000 to $74,999"="Middle-income",
+                                           "$75,000 to $99,999"="Middle-income",
+                                           "$100,000 to $ 124,999"="Middle-income",
+                                           "$125,000 and more"="Upper-income"))
+                                           
+
+#education
+
+census_data$education<-revalue(census_data$education, c("Less than high school diploma or its equivalent"="Below Upper-Secondary",
+                                                        "High school diploma or a high school equivalency certificate"="Upper Secondary",
+                                                        "Trade certificate or diploma"="Tertiary Education",
+                                                        "College, CEGEP or other non-university certificate or di..."="Tertiary Education",
+                                                        "University certificate or diploma below the bachelor's level"="Tertiary Education",
+                                                        "University certificate, diploma or degree above the bach..."="Tertiary Education",
+                                                        "Bachelor's degree (e.g. B.A., B.Sc., LL.B.)"="Tertiary Education"))
+
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # STA304_Final_Project
-The repo of STA304 Final Project
+
+This repo contains rmd file for report of the 2019 Canadian General Election. It was created by Leyi Wang. The purpose is to create a repo that build statistical model to predict the outcome of election if everyone has voted.
+
+The sections of repo are : Report, Codes and cleaned data.
+
+Outputs contains data and result of our prediction of Canadian 2019 Election outcome if everyone has voted.
+
+- Codes
+
+Rscripts contain how we cleaned and modified the data.
+
+1. Cleaning data: It contains three Rscripts: 
+
+  (1). Cleaning Survey Data.R
+  
+  (2). Cleaning Census Data.R 
+  
+  (3). Cleaning Post Stratification Data.R
+  
+
+2. Modeling and prediction analysis.R
+
+- Report: 
+
+It contains the the report that is finally presented. 
+
+1. Final_Report.rmd
+2. Final_Report.pdf
+
+
+
